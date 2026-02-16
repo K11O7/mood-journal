0b77e264ccf3e5fa5681fcfb23e127da54c3723b
K11O7 <Krishchoukse1107@gmail.com>
2026-02-16 21:45:28 +0530

    postgresql connection

--- journal/gemini_utils.py
+++ journal/gemini_utils.py
@@ -2,8 +2,7 @@ import google.genai as genai
 from django.conf import settings
 
 client = genai.Client(api_key=settings.GEMINI_API_KEY)
-for m in client.models.list():
-    print(m.name)
+
 model = "gemini-2.5-flash" # free + fast
 
 def analyze_mood(entry_text: str) -> str:
