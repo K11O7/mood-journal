ae3e39f321b4ad0d84aa2b5004ef3f2209965ace
Krish Choukse <166479302+K11O7@users.noreply.github.com>
2026-03-04 21:48:38 +0530

    Update settings.py to set debug to false

--- moodjournal/settings.py
+++ moodjournal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
